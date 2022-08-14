719cd312162185a5fdd4814745f2f9fb93aa6131
Lee Baesler <lee.baesler@gmail.com>
2022-08-14 13:23:41 -0500

    save

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,12 @@
-console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+const basket = [];{
+function addItem(){
+    if(basket = addItem)
+    return true
+}console.log(true)
+}
+function listItems (basket){
+    let i = 0;
+    console.log([i]);
+}
